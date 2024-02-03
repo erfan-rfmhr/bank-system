a94b13bdca631b6cd342d8df73716fd1d5e7f507
Ehsan <ehsanmelm82@gmail.com>
2024-02-03 19:28:06 +0330

    feat : define url for transactions

--- config/urls.py
+++ config/urls.py
@@ -29,5 +29,6 @@ urlpatterns += [
         SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui",
     ),
     path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.jwt'))
+    path('auth/', include('djoser.urls.jwt')),
+    path('' , include('transaction.urls'))
 ]
