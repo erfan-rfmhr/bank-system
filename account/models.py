3285259658827c5cab03b19a7e90126524ffdb51
Erfan Arefmehr <erfan.arefmehr@gmail.com>
2024-02-03 15:17:48 +0330

    update(models): change balance column type to positive integer. add created_at column in TransactionModel and AccountModel

--- account/models.py
+++ account/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.conf import settings
 from django.contrib import admin
 # Create your models here.
-
 class AccountOwenrModel(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL , on_delete = models.CASCADE)
     phone_number = models.CharField(max_length = 11 )
@@ -17,7 +16,7 @@ class AccountModel(models.Model):
         ('jari' , 'jari'),
     ]
     user = models.ForeignKey(AccountOwenrModel , on_delete = models.CASCADE)
-    balance = models.CharField(max_length =255)
+    balance = models.PositiveIntegerField(default=0)
     type = models.CharField(max_length =255 , choices = ACCOUNT_TYPE_CHOICES)
     is_blocked = models.BooleanField(default =False)
-
+    created_at = models.DateTimeField(auto_now_add=True)
--- transaction/models.py
+++ transaction/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 from account.models import  AccountModel
-
 # Create your models here.
 
 class TransactionModel(models.Model):
     sender = models.ForeignKey(AccountModel , on_delete = models.CASCADE , related_name =   'sender')
     receiver = models.ForeignKey(AccountModel , on_delete = models.CASCADE , related_name = 'receiver')
-    balance = models.CharField(max_length = 255)
+    balance = models.PositiveIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
