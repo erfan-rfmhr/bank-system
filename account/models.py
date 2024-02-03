62fcc45e09791884e8ca819b7010d5c5c2a076ed
Ehsan <ehsanmelm82@gmail.com>
2024-02-03 12:46:39 +0330

    feat : create Account Model

--- account/models.py
+++ account/models.py
@@ -7,3 +7,12 @@ class AccountOwenr(models.Model):
     User = models.OneToOneField(settings.USER_AUTH_MODEL , on_delete = models.CASCADE)
     phone = models.CharField(max_length = 11)
 
+class Account(models.Model):
+    ACCOUNT_TYPE_CHOICES = [
+        ('seporde' , 'seporde'),
+        ('jari' , 'jari'),
+    ]
+    user = models.ForeignKey(AccountOwenr , on_delete = models.CASCADE)
+    balance = models.CharField(max_length =255)
+    type = models.CharField(max_length =255 , choices = ACCOUNT_TYPE_CHOICES)
+    
