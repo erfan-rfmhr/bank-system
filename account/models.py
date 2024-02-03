7c3239c5be1115a8b9d0682c2f0452b4962bd3dc
Ehsan <ehsanmelm82@gmail.com>
2024-02-03 12:46:19 +0330

    feat : create AccountOwner model

--- account/models.py
+++ account/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+class AccountOwenr(models.Model):
+    User = models.OneToOneField(settings.USER_AUTH_MODEL , on_delete = models.CASCADE)
+    phone = models.CharField(max_length = 11)
+
