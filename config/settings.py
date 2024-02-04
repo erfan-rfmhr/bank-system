bfebcbbf314f6e5b0850c2ef5092139194435163
Erfan Arefmehr <erfan.arefmehr@gmail.com>
2024-02-04 21:08:50 +0330

    update(simplejwt settings): set Bearer as header prefix

--- config/settings.py
+++ config/settings.py
@@ -144,7 +144,7 @@ REST_FRAMEWORK = {
 
 # SIMPLE JWT
 SIMPLE_JWT = {
-    'AUTH_HEADER_TYPES': ('JWT',),
+    'AUTH_HEADER_TYPES': ('Bearer',),
     "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=10),
 }
