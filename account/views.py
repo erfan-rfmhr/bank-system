e2b305f6c50e556595628254ed8f2e075af448b2
Erfan Arefmehr <erfan.arefmehr@gmail.com>
2024-02-04 22:43:37 +0330

    feat(account.views.AccountOwnerUpdateView): create AccountOwnerUpdateView for updating account owner

--- account/views.py
+++ account/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.generics import UpdateAPIView
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .models import AccountOwenrModel
+from .serializers import AccountOwnerSerializer
+
+
+class AccountOwnerUpdateView(UpdateAPIView):
+    serializer_class = AccountOwnerSerializer
+    queryset = AccountOwenrModel.objects.all()
+    permission_classes = [IsAuthenticated]
+    lookup_field = 'user__username'
+    lookup_url_kwarg = 'username'
