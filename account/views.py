36b739f127824a692c23ab5dbd50d3ccb7f780ec
Erfan Arefmehr <erfan.arefmehr@gmail.com>
2024-02-05 18:17:39 +0330

    feat(account.AccountJariCreateView): create a view for creating a jari account

--- account/views.py
+++ account/views.py
@@ -1,8 +1,8 @@
-from rest_framework.generics import UpdateAPIView
+from rest_framework.generics import UpdateAPIView, CreateAPIView
 from rest_framework.permissions import IsAuthenticated
-
-from .models import AccountOwenrModel
-from .serializers import AccountOwnerSerializer
+from rest_framework.response import Response
+from .models import AccountOwenrModel, AccountModel
+from .serializers import AccountOwnerSerializer, AccountJariCreationSerializer
 
 
 class AccountOwnerUpdateView(UpdateAPIView):
@@ -11,3 +11,17 @@ class AccountOwnerUpdateView(UpdateAPIView):
     permission_classes = [IsAuthenticated]
     lookup_field = 'user__username'
     lookup_url_kwarg = 'username'
+
+
+class AccountJariCreateView(CreateAPIView):
+    serializer_class = AccountJariCreationSerializer
+    queryset = AccountModel.objects.all()
+    permission_classes = [IsAuthenticated]
+
+    def create(self, request, *args, **kwargs):
+        user = request.user
+        account = AccountModel.objects.filter(user=user.account_owner, type='jari')
+        if not account.exists():
+            return super().create(request, *args, **kwargs)
+        return Response({'detail': 'شما در حال حاضر یک حساب جاری دارید.'}, status=400)
+
