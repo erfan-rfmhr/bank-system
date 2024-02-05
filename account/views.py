eb73cb7660f913d27e43d0f3d147ffb9b3e7c376
Erfan Arefmehr <erfan.arefmehr@gmail.com>
2024-02-05 18:35:54 +0330

    feat(account.views): create AccountSepordeCreateView for handling seporde creation

--- account/views.py
+++ account/views.py
@@ -1,8 +1,9 @@
 from rest_framework.generics import UpdateAPIView, CreateAPIView
+from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from .models import AccountOwenrModel, AccountModel
-from .serializers import AccountOwnerSerializer, AccountJariCreationSerializer
+from .serializers import AccountOwnerSerializer, AccountJariCreationSerializer, AccountSepordeCreationSerializer
 
 
 class AccountOwnerUpdateView(UpdateAPIView):
@@ -25,3 +26,14 @@ class AccountJariCreateView(CreateAPIView):
             return super().create(request, *args, **kwargs)
         return Response({'detail': 'شما در حال حاضر یک حساب جاری دارید.'}, status=400)
 
+
+class AccountSepordeCreateView(APIView):
+    def get(self, request, *args, **kwargs):
+        user = request.user
+        account = AccountModel.objects.filter(user=user.account_owner, type='seporde')
+        if account.exists():
+            return Response({'detail': 'شما در حال حاضر یک حساب سپرده دارید.'}, status=400)
+
+        account = AccountModel.objects.create(user=user.account_owner, type='seporde')
+        serializer = AccountSepordeCreationSerializer(account)
+        return Response(serializer.data, status=201)
