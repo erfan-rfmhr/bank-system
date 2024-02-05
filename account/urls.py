9df5ef73c74fdea39053c8e9217272f7f522053d
Erfan Arefmehr <erfan.arefmehr@gmail.com>
2024-02-05 18:18:20 +0330

    feat(account.urls): add jari/create url to account urls

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,8 @@
-# urls.py
-
 from django.urls import path
+
 from . import views
+
 urlpatterns = [
     path('profile/<str:username>/update/', views.AccountOwnerUpdateView.as_view()),
+    path('jari/create/', views.AccountJariCreateView.as_view()),
 ]
